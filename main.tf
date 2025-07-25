8b956d5ea9091fa314982f68ed736c80811933d3
Your Name <your-email@example.com>
2025-07-26 02:52:31 +0530

    updated

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ data "azurerm_resource_group" "existing" {
 
 # Virtual network 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "windows-vnettest"
+  name                = "windows-vnetfinal"
   address_space       = ["10.0.0.0/16"]
   location            = data.azurerm_resource_group.existing.location
   resource_group_name = data.azurerm_resource_group.existing.name
@@ -19,7 +19,7 @@ resource "azurerm_virtual_network" "vnet" {
  
 # Subnet
 resource "azurerm_subnet" "subnet" {
-  name                 = "windowssubnettest"
+  name                 = "windowssubnetfinal"
   resource_group_name  = data.azurerm_resource_group.existing.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
@@ -35,7 +35,7 @@ resource "azurerm_subnet" "subnet" {
  
 # Network interface
 resource "azurerm_network_interface" "nic" {
-  name                = "windows-nictest"
+  name                = "windows-nicfinal"
   location            = data.azurerm_resource_group.existing.location
   resource_group_name = data.azurerm_resource_group.existing.name
  
@@ -49,7 +49,7 @@ resource "azurerm_network_interface" "nic" {
  
 # VM creation
 resource "azurerm_windows_virtual_machine" "vm" {
-  name                = "myWindowsVMtest"
+  name                = "myWindowsVMfinal"
   resource_group_name = data.azurerm_resource_group.existing.name
   location            = data.azurerm_resource_group.existing.location
   size                = "Standard_D2s_v3"
